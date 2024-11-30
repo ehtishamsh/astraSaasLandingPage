ccbbdefc13311a0e887dba449b4dc40b9b197317
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-11-30 07:41:49 +0500

    feature

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,6 @@
 import AI from "../components/home/AI";
+import Feature from "../components/home/Feature";
+import Features from "../components/home/Features";
 import Graphs from "../components/home/Graphs";
 import HeroSection from "../components/home/HeroSection";
 function Home() {
@@ -7,6 +9,8 @@ function Home() {
       <HeroSection />
       <AI />
       <Graphs />
+      <Feature />
+      <Features />
     </>
   );
 }
