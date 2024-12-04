77de91387db22ee53fbc35f5e884fd37f19d85c9
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-12-04 07:55:10 +0500

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Navbar />
       <div className="bg-mainColor-950">
         <Outlet />
+        <Footer />
       </div>
     </>
   );
