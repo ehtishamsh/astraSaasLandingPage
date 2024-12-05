f8f2204e0280c57d02dbea568d2f1c772d866943
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-12-05 06:02:41 +0500

    changed and added new styles

--- src/components/home/Howitworks.tsx
+++ src/components/home/Howitworks.tsx
@@ -5,7 +5,7 @@ function Howitworks() {
     <div className="relative">
       <div className="  absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]  bg-mainColor-600 w-96 h-96 rounded-full z-10 blur-[14rem]"></div>
 
-      <div className="mt-28 max-w-7xl mx-auto text-center pb-14 px-4  relative z-20">
+      <div className="mt-20 max-w-7xl mx-auto text-center pb-14 px-4  relative z-20">
         <span className="text-mainColor-100 bg-mainColor-800 rounded-[4px] py-1 px-4 text-sm border border-mainColor-200/10">
           HOW IT WORKS
         </span>
@@ -22,40 +22,40 @@ function Howitworks() {
             <Link
               reloadDocument
               to="/"
-              className="px-5 py-2 rounded-[8px] flex items-center justify-center gap-1 text-base text-mainColor-100 bg-gradient-to-b from-[#B266FF] to-[#9C39FF] border border-mainColor-300"
+              className="px-5 py-2 rounded-[8px] flex items-center justify-center gap-1 text-base text-mainColor-100 hover:bg-gradient-to-b transition-all duration-200 hover:from-[#a245ff] hover:to-[#9024fb] bg-gradient-to-b from-[#B266FF] to-[#9C39FF] border border-mainColor-300"
             >
               Get Started
             </Link>
             <Link
               reloadDocument
               to="/"
-              className="px-5 py-2 rounded-[8px] flex items-center justify-center gap-1 text-base text-mainColor-100 bg-gradient-to-b from-[#b469ff53] to-transparent border border-mainColor-300"
+              className="px-5 py-2 rounded-[8px] flex items-center justify-center gap-1 text-base text-mainColor-100 hover:bg-gradient-to-b transition-all duration-200 hover:from-[#9b38ff53] hover:to-transparent bg-gradient-to-b from-[#b469ff53] to-transparent border border-mainColor-300"
             >
               Learn More
             </Link>
           </div>
         </div>
         <div className="mt-2 grid grid-cols-3 gap-6 pt-24">
-          <div className="p-8 rounded-2xl text-left bg-mainColor-800/40 border inner-shadow border-borderMainColor">
+          <div className="p-5 rounded-2xl text-left hover:bg-mainColor-900/40 bg-mainColor-900 transition-all duration-200  border inner-shadow border-borderMainColor">
             <img
               src="/howitworks.svg"
               alt="How it works"
               className="max-w-6 mb-8"
             />
-            <h3 className="text-2xl font-medium mb-4 text-mainColor-100">
+            <h3 className="text-xl font-medium mb-4 text-mainColor-100">
               AI-Powered Insights
             </h3>
             <p className="text-mainColor-200/50 text-base">
               Gain actionable intelligence with advanced AI algorithms.
             </p>
           </div>
-          <div className="p-8 rounded-2xl text-left bg-mainColor-800/40 border inner-shadow border-borderMainColor">
+          <div className="p-5 rounded-2xl text-left hover:bg-mainColor-900/40 bg-mainColor-900 transition-all duration-200  border inner-shadow border-borderMainColor">
             <img
               src="/howitworks.svg"
               alt="How it works"
               className="max-w-6 mb-8"
             />
-            <h3 className="text-2xl font-medium mb-4 text-mainColor-100">
+            <h3 className="text-xl font-medium mb-4 text-mainColor-100">
               Real-Time Dashboards
             </h3>
             <p className="text-mainColor-200/50 text-base">
@@ -63,26 +63,26 @@ function Howitworks() {
               dashboards.
             </p>
           </div>
-          <div className="p-8 rounded-2xl text-left bg-mainColor-800/40 border inner-shadow border-borderMainColor">
+          <div className="p-5 rounded-2xl text-left hover:bg-mainColor-900/40 bg-mainColor-900 transition-all duration-200  border inner-shadow border-borderMainColor">
             <img
               src="/howitworks.svg"
               alt="How it works"
               className="max-w-6 mb-8"
             />
-            <h3 className="text-2xl font-medium mb-4 text-mainColor-100">
+            <h3 className="text-xl font-medium mb-4 text-mainColor-100">
               Automated Reporting
             </h3>
             <p className="text-mainColor-200/50 text-base">
               Save time and ensure accuracy with automated report generation.
             </p>
           </div>
-          <div className="p-8 rounded-2xl text-left bg-mainColor-800/40 border inner-shadow border-borderMainColor">
+          <div className="p-5 rounded-2xl text-left hover:bg-mainColor-900/40 bg-mainColor-900 transition-all duration-200  border inner-shadow border-borderMainColor">
             <img
               src="/howitworks.svg"
               alt="How it works"
               className="max-w-6 mb-8"
             />
-            <h3 className="text-2xl font-medium mb-4 text-mainColor-100">
+            <h3 className="text-xl font-medium mb-4 text-mainColor-100">
               Comprehensive Data
             </h3>
             <p className="text-mainColor-200/50 text-base">
@@ -90,26 +90,26 @@ function Howitworks() {
               time.
             </p>
           </div>
-          <div className="p-8 rounded-2xl text-left bg-mainColor-800/40 border inner-shadow border-borderMainColor">
+          <div className="p-5 rounded-2xl text-left hover:bg-mainColor-900/40 bg-mainColor-900 transition-all duration-200  border inner-shadow border-borderMainColor">
             <img
               src="/howitworks.svg"
               alt="How it works"
               className="max-w-6 mb-8"
             />
-            <h3 className="text-2xl font-medium mb-4 text-mainColor-100">
+            <h3 className="text-xl font-medium mb-4 text-mainColor-100">
               Customizable Dashboards
             </h3>
             <p className="text-mainColor-200/50 text-base">
               Tailor your dashboards to fit your business needs.
             </p>
           </div>
-          <div className="p-8 rounded-2xl text-left bg-mainColor-800/40 border inner-shadow border-borderMainColor">
+          <div className="p-5 rounded-2xl text-left hover:bg-mainColor-900/40 bg-mainColor-900 transition-all duration-200  border inner-shadow border-borderMainColor">
             <img
               src="/howitworks.svg"
               alt="How it works"
               className="max-w-6 mb-8"
             />
-            <h3 className="text-2xl font-medium mb-4 text-mainColor-100">
+            <h3 className="text-xl font-medium mb-4 text-mainColor-100">
               AI-Powered Insights
             </h3>
             <p className="text-mainColor-200/50 text-base">
