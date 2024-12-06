8ccf5ef269f50025b3962326c76c6751b286c4da
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-12-06 08:05:30 +0500

    card data

---
+++ src/components/home/Cardbg.ts
@@ -0,0 +1,30 @@
+export const cardData = [
+  {
+    imgSrc: "/bento1.png",
+    imgAlt: "Customizable Dashboard",
+    title: "Customizable Dashboards",
+    description:
+      "Tailor dashboards to display the metrics and KPIs most relevant to your business needs.",
+  },
+  {
+    imgSrc: "/bento2.png",
+    imgAlt: "Automated Reporting",
+    title: "Automated Reporting",
+    description:
+      "Generate comprehensive reports automatically, saving time and ensuring accuracy.",
+  },
+  {
+    imgSrc: "/bento3.png",
+    imgAlt: "Integrations",
+    title: "Integrations",
+    description:
+      "Stay informed with real-time alerts and notifications about significant changes or anomalies in vendor performance.",
+  },
+  {
+    imgSrc: "/bento4.png",
+    imgAlt: "Monthly Tracking Graphs",
+    title: "Monthly Tracking Graphs",
+    description:
+      "Visualize vendor activities and performance with intuitive graphs updated every month.",
+  },
+];
