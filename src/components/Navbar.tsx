b904ea40bd1b7dfbbaab345ac4fc3736d294e4f1
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2024-12-16 06:09:14 +0500

    mobile nav structure

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom";
 import { IoChevronForwardOutline } from "react-icons/io5";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
+import { IoIosStarOutline } from "react-icons/io";
+
+import Hamburger from "hamburger-react";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header className=" fixed top-0 left-0 right-0 bg-mainColor-900/30 backdrop-blur-md  z-[999]">
-      <nav className="max-w-7xl mx-auto px-4 py-4 flex items-center">
+      <nav className="max-w-7xl mx-auto px-4 py-4 flex max-lg:justify-between items-center">
         <Link reloadDocument to="/" className="mr-14">
           <img src="/logo.svg" alt="Logo" />
         </Link>
-        <div className="flex justify-between items-center w-full">
+        <div className="flex  max-lg:hidden justify-between items-center w-full">
           <ul className="flex gap-8 items-center text-base text-mainColor-100">
             <li>
               <Link
@@ -67,7 +74,45 @@ function Navbar() {
             </span>
           </Link>
         </div>
+        <div className="lg:hidden z-[9999] flex items-center">
+          <Hamburger
+            color="#fff"
+            toggle={setIsOpen}
+            toggled={isOpen}
+            onToggle={() => setIsOpen(!isOpen)}
+          />
+        </div>
       </nav>
+      {isOpen && (
+        <motion.div
+          className="fixed top-0 left-0 right-0 bottom-0 bg-mainColor-900 z-[999] h-screen backdrop-blur-md"
+          initial={{ x: "100%" }}
+          animate={{ x: 0, transition: { duration: 0.3, ease: "easeInOut" } }}
+        >
+          <div className="flex flex-col gap-4 text-mainColor-100 items-center justify-start pt-20 px-6 h-full">
+            <Link
+              reloadDocument
+              to="/features"
+              className="hover:bg-mainColor-700 text-lg rounded-[8px] flex gap-2 px-4 py-2 items-center text-start w-full transition-all duration-200"
+            >
+              <span>
+                <IoIosStarOutline size={20} />
+              </span>
+              <span>AI & Graph</span>
+            </Link>
+            <Link
+              reloadDocument
+              to="/features"
+              className="hover:bg-mainColor-700 text-lg rounded-[8px] flex gap-2 px-4 py-2 items-center text-start w-full transition-all duration-200"
+            >
+              <span>
+                <IoMdCheckmarkCircleOutline size={20} />
+              </span>
+              <span>Features</span>
+            </Link>
+          </div>
+        </motion.div>
+      )}
     </header>
   );
 }
